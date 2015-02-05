aa8e7a6c775f725834bf4815a0e894732f9df2e1
bhans_000 <bhans_000@Ben>
2015-02-05 12:15:00 -0600

    Layout the general look of the chess board

---
+++ src/chess/Board.java
@@ -0,0 +1,75 @@
+
+package chess;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JPanel;
+
+
+public class Board extends JPanel {
+    
+ 
+    
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D image = (Graphics2D) g;
+	image.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        image.setColor(Color.black);
+       
+        image.fillRect(0, 0, getWidth(), getHeight());
+       
+        image.setColor(Color.orange);       
+        image.fillRect(0, 0, (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect((getWidth() / 8) * 2, 0, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 4, 0, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 6, 0, (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect((getWidth() / 8),    (getHeight() / 8), (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 3, (getHeight() / 8), (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 5, (getHeight() / 8), (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 7, (getHeight() / 8), (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect(0, (getHeight() / 8)* 2, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 2, (getHeight() / 8)* 2, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 4, (getHeight() / 8)* 2, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 6, (getHeight() / 8)* 2, (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect((getWidth() / 8),    (getHeight() / 8) * 3, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 3, (getHeight() / 8) * 3, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 5, (getHeight() / 8) * 3, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 7, (getHeight() / 8) * 3, (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect(0, (getHeight() / 8)* 4, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 2, (getHeight() / 8)* 4, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 4, (getHeight() / 8)* 4, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 6, (getHeight() / 8)* 4, (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect((getWidth() / 8),    (getHeight() / 8) * 5, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 3, (getHeight() / 8) * 5, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 5, (getHeight() / 8) * 5, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 7, (getHeight() / 8) * 5, (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect(0, (getHeight() / 8)* 6, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 2, (getHeight() / 8)* 6, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 4, (getHeight() / 8)* 6, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8) * 6, (getHeight() / 8)* 6, (getWidth() / 8), (getHeight() / 8));
+        
+        image.fillRect((getWidth() / 8),    (getHeight() / 8) * 7, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 3, (getHeight() / 8) * 7, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 5, (getHeight() / 8) * 7, (getWidth() / 8), (getHeight() / 8));
+        image.fillRect((getWidth() / 8)* 7, (getHeight() / 8) * 7, (getWidth() / 8), (getHeight() / 8));
+        
+        
+        
+        
+}
+    
+    
+}
---
+++ src/chess/Window.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Window extends JPanel {
+
+
+    
+    public static void main(String[] args) throws InterruptedException {
+        JFrame Window = new JFrame("Chess");
+        Board Game = new Board();
+        Window.add(Game);
+        Window.setSize(300, 300);
+        Window.setVisible(true);
+        Window.setLocation(150, 150);
+        Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+       
+        
+       
+    }
+
+   
+    
+
+}
+
+
+    
+  
